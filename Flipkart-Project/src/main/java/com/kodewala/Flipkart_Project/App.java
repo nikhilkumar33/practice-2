d7ac21c6c13511909a70d3089e56208aa5a8f6ad
Nikhil Kumar Jena <142205210+nikhilkumar33@users.noreply.github.com>
2026-01-05 11:32:56 +0530

    Update App.java

--- Flipkart-Project/src/main/java/com/kodewala/Flipkart_Project/App.java
+++ Flipkart-Project/src/main/java/com/kodewala/Flipkart_Project/App.java
@@ -1,14 +1,10 @@
 package com.kodewala.Flipkart_Project;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
         System.out.println("Hello Kodewala!");
+        System.out.println("Btm");
     }
 }
